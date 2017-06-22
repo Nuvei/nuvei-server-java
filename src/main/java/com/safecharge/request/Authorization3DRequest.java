800a1fdcef4a518922aa39a997d9595440928634
Nikola Dichev <nikolad@safecharge.com>
2017-06-22 16:28:53 +0300

    - added isDynamic3D to Auth3D requests.

--- src/main/java/com/safecharge/request/Authorization3DRequest.java
+++ src/main/java/com/safecharge/request/Authorization3DRequest.java
@@ -1,6 +1,10 @@
 package com.safecharge.request;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
 import com.safecharge.request.builder.SafechargeCCBuilder;
+import com.safecharge.util.APIConstants;
 import com.safecharge.util.Constants;
 import com.safecharge.util.ValidChecksum;
 import com.safecharge.util.ValidationUtil;
@@ -14,8 +18,19 @@ import com.safecharge.util.ValidationUtil;
 @ValidChecksum(orderMappingName = Constants.ChecksumOrderMapping.API_GENERIC_CHECKSUM_MAPPING) public class Authorization3DRequest
         extends SafechargeCCRequest {
 
+    @NotNull(message = "isDynamic3D parameter is mandatory!") @Pattern(regexp = APIConstants.IS_DYNAMIC_3D_REGEX,
+                                                                       message = "isDynamic3D value is invalid") private String isDynamic3D;
+
     public static Builder builder() {
         return new Builder();
+    }
+
+    public String getIsDynamic3D() {
+        return isDynamic3D;
+    }
+
+    public void setIsDynamic3D(String isDynamic3D) {
+        this.isDynamic3D = isDynamic3D;
     }
 
     @Override public String toString() {
@@ -27,8 +42,15 @@ import com.safecharge.util.ValidationUtil;
 
     public static class Builder extends SafechargeCCBuilder<Builder> {
 
+        private String isDynamic3D;
+
+        public void addIsDynamic3D(String isDynamic3D) {
+            this.isDynamic3D = isDynamic3D;
+        }
+
         @Override public SafechargeRequest build() {
             Authorization3DRequest request = new Authorization3DRequest();
+            request.setIsDynamic3D(isDynamic3D);
             return ValidationUtil.validate(super.build(request));
         }
     }
