4f9350105cf425fdc3d504ffc6e5b1db16b59d2f
Bozhidar Shumanov <32544587+DopplerEffect666@users.noreply.github.com>
2017-11-30 11:32:03 +0200

    Slight refactoring so when building CardTokenization request no merchantId and merchantSiteId should be provided. (#13)

--- src/main/java/com/safecharge/request/builder/SafechargeBaseBuilder.java
+++ src/main/java/com/safecharge/request/builder/SafechargeBaseBuilder.java
@@ -2,6 +2,7 @@ package com.safecharge.request.builder;
 
 import javax.validation.ConstraintViolationException;
 
+import com.safecharge.model.MerchantBaseInfo;
 import com.safecharge.model.MerchantInfo;
 import com.safecharge.request.SafechargeBaseRequest;
 import com.safecharge.request.SafechargeRequest;
@@ -14,15 +15,13 @@ import com.safecharge.util.RequestUtils;
  * <p>
  * A base builder class for a request to Safecharge.
  *
- * @author <a mailto:nikolad@safecharge.com>Nikola Dichev</a>
- * @see SafechargeCCBuilder
- * @see SafechargeOrderBuilder
- * @see SafechargeTransactionBuilder
+ * @author <a mailto:bozhidarsh@safecharge.com>Bozhidar Shumanov</a>
+ * @see SafechargeBuilder
  * @since 2/23/2017
  */
-public abstract class SafechargeBuilder<T extends SafechargeBuilder<T>> {
+public abstract class SafechargeBaseBuilder<T extends SafechargeBaseBuilder<T>> {
 
-    protected MerchantInfo merchantInfo;
+    protected MerchantBaseInfo merchantInfo;
     private String clientRequestId;
     private String internalRequestId;
     private String sessionToken;
@@ -64,12 +63,12 @@ public abstract class SafechargeBuilder<T extends SafechargeBuilder<T>> {
     /**
      * Adds required merchant info to the request.
      *
-     * @param merchantInfo A {@link MerchantInfo} object to get the merchant info from, when building the request
+     * @param merchantInfo A {@link MerchantBaseInfo} object to get the merchant info from, when building the request
      * @return this object
-     * @see MerchantInfo
+     * @see MerchantBaseInfo
      * @see SafechargeBuilder#addMerchantInfo(String, String, String, String, Constants.HashAlgorithm)
      */
-    public T addMerchantInfo(MerchantInfo merchantInfo) {
+    public T addMerchantInfo(MerchantBaseInfo merchantInfo) {
         this.merchantInfo = merchantInfo;
         return (T) this;
     }
@@ -78,41 +77,37 @@ public abstract class SafechargeBuilder<T extends SafechargeBuilder<T>> {
      * Adds required merchant info to the request.
      *
      * @param merchantKey    The secret merchant key obtained by the Merchant during integration process with Safecharge
-     * @param merchantId     Merchant id in the Safecharge's system
-     * @param merchantSiteId Merchant site id in the Safecharge's system
      * @param serverHost     The Safecharge's server address to send the request to
      * @param hashAlgorithm  The hashing algorithm used to generate the checksum
      * @return this object
-     * @see MerchantInfo
-     * @see SafechargeBuilder#addMerchantInfo(MerchantInfo)
+     * @see MerchantBaseInfo
+     * @see SafechargeBaseBuilder#addMerchantInfo(MerchantBaseInfo)
      */
-    public T addMerchantInfo(String merchantKey, String merchantId, String merchantSiteId, String serverHost, Constants.HashAlgorithm hashAlgorithm) {
-        this.merchantInfo = new MerchantInfo(merchantKey, merchantId, merchantSiteId, serverHost, hashAlgorithm);
+    public T addMerchantInfo(String merchantKey, String serverHost, Constants.HashAlgorithm hashAlgorithm) {
+        this.merchantInfo = new MerchantBaseInfo(merchantKey, serverHost, hashAlgorithm);
         return (T) this;
     }
 
     /**
      * Adds the common data, collected by this builder. The intent of this method is to be used from the derived classes.
      *
-     * @param safechargeRequest an already created request of type <{@code T} extends {@link SafechargeRequest}>
+     * @param safechargeBaseRequest an already created request of type <{@code T} extends {@link SafechargeBaseRequest}>
      * @param <S>               type parameter
-     * @return the passed {@code safechargeRequest} filled with the data from this builder
+     * @return the passed {@code safechargeBaseRequest} filled with the data from this builder
      */
-    protected <S extends SafechargeRequest> S build(S safechargeRequest) {
+    protected <S extends SafechargeBaseRequest> S build(S safechargeBaseRequest) {
 
         String timestamp = RequestUtils.calculateTimestamp();
-        safechargeRequest.setMerchantId(merchantInfo != null ? merchantInfo.getMerchantId() : null);
-        safechargeRequest.setMerchantSiteId(merchantInfo != null ? merchantInfo.getMerchantSiteId() : null);
-        safechargeRequest.setServerHost(merchantInfo != null ? merchantInfo.getServerHost() : null);
-        safechargeRequest.setSessionToken(sessionToken);
-        safechargeRequest.setTimeStamp(timestamp);
-        safechargeRequest.setClientRequestId(clientRequestId);
-        safechargeRequest.setInternalRequestId(internalRequestId);
-        safechargeRequest.setChecksum(
-                ChecksumUtils.calculateChecksum(safechargeRequest, merchantInfo != null ? merchantInfo.getMerchantKey() : "", Constants.CHARSET_UTF8,
+        safechargeBaseRequest.setServerHost(merchantInfo != null ? merchantInfo.getServerHost() : null);
+        safechargeBaseRequest.setSessionToken(sessionToken);
+        safechargeBaseRequest.setTimeStamp(timestamp);
+        safechargeBaseRequest.setClientRequestId(clientRequestId);
+        safechargeBaseRequest.setInternalRequestId(internalRequestId);
+        safechargeBaseRequest.setChecksum(
+                ChecksumUtils.calculateChecksum(safechargeBaseRequest, merchantInfo != null ? merchantInfo.getMerchantKey() : "", Constants.CHARSET_UTF8,
                         merchantInfo != null ? merchantInfo.getHashAlgorithm() : null));
 
-        return safechargeRequest;
+        return safechargeBaseRequest;
     }
 
     /**
