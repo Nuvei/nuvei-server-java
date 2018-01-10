e5fe2ebaed6c5ec77c1c18c7b9d753e79cffb35d
bozhidarsh <bozhidarsh@safecharge.com>
2018-01-10 18:50:03 +0200

    Some MORE code forgotten to be added in order to handle the 3 API methods for user management.

--- src/main/java/com/safecharge/request/UpdateUserRequest.java
+++ src/main/java/com/safecharge/request/UpdateUserRequest.java
@@ -7,4 +7,17 @@ import com.safecharge.util.ValidChecksum;
 @ValidChecksum(orderMappingName = Constants.ChecksumOrderMapping.MODIFY_CASHIER_USER)
 public class UpdateUserRequest extends CashierUserRequest {
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public UpdateUserRequest(Builder b) {
+        super(b);
+    }
+
+    public static class Builder extends CashierUserRequest.Builder {
+        public UpdateUserRequest build() {
+            return new UpdateUserRequest(this);
+        }
+    }
 }
