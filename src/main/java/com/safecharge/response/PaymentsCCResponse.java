069152f6951869af2851fdfc06b4365832d5da03
bozhidarsh <bozhidarsh@safecharge.com>
2018-08-07 10:04:53 +0300

    - paymentCC, dynamic3D, payment3D output parametes CVV2Reply and AVSCode.

--- src/main/java/com/safecharge/response/PaymentsCCResponse.java
+++ src/main/java/com/safecharge/response/PaymentsCCResponse.java
@@ -6,6 +6,9 @@ public class PaymentsCCResponse extends PaymentsResponse {
 
     protected PartialApprovalDetails partialApprovalDetails;
 
+    protected String CVV2Reply;
+    protected String AVSCode;
+
     public PartialApprovalDetails getPartialApprovalDetails() {
         return partialApprovalDetails;
     }
@@ -14,12 +17,32 @@ public class PaymentsCCResponse extends PaymentsResponse {
         this.partialApprovalDetails = partialApprovalDetails;
     }
 
+    public String getCVV2Reply() {
+        return CVV2Reply;
+    }
+
+    public void setCVV2Reply(String CVV2Reply) {
+        this.CVV2Reply = CVV2Reply;
+    }
+
+    public String getAVSCode() {
+        return AVSCode;
+    }
+
+    public void setAVSCode(String AVSCode) {
+        this.AVSCode = AVSCode;
+    }
+
     @Override
     public String toString() {
         StringBuilder sBuilder = new StringBuilder("PaymentsCCResponse [");
         sBuilder.append(super.toString());
         sBuilder.append(", partialApprovalDetails=");
         sBuilder.append(partialApprovalDetails);
+        sBuilder.append(", CVV2Reply=");
+        sBuilder.append(CVV2Reply);
+        sBuilder.append(", AVSCode=");
+        sBuilder.append(AVSCode);
         sBuilder.append("]");
         return sBuilder.toString();
     }
