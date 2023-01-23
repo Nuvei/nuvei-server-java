436c57b091d8866087642aec5567af3027b7d763
radostin.tonev <radostin.tonev@nuvei.com>
2023-01-23 15:33:15 +0200

    536799 Rename dateOfBirth to birthdate in UserDetails

--- src/main/java/com/safecharge/model/UserDetails.java
+++ src/main/java/com/safecharge/model/UserDetails.java
@@ -15,22 +15,22 @@ import com.safecharge.util.ValidChecksum;
 @ValidChecksum(orderMappingName = Constants.ChecksumOrderMapping.USER_DETAILS)
 public class UserDetails extends CommonUserDetailsInfo {
 
-    private String dateOfBirth;
+    private String birthdate;
 
-    public String getDateOfBirth() {
-        return dateOfBirth;
+    public String getBirthdate() {
+        return birthdate;
     }
 
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
     }
 
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("UserDetails{");
         sb.append(super.toString());
-        sb.append(", dateOfBirth='")
-                .append(dateOfBirth)
+        sb.append(", Birthdate='")
+                .append(birthdate)
                 .append('\'');
         sb.append('}');
 
